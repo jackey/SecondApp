49af24098a2130f2794d3c56de9ca18b7b58de2e
jackey <jackey.chen@sucel-it.com>
2015-12-11 11:24:43 +0800

    Initial Commit

---
+++ SecondApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondApp
+//
+//  Created by JackyChen's iMac on 11/12/2015.
+//  Copyright © 2015 Jackey's iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecondApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecondApp
+//
+//  Created by JackyChen's iMac on 11/12/2015.
+//  Copyright © 2015 Jackey's iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
